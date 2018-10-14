df97dfba597fd38cb6dbfb202550e13b679454f1
incrinage <incrinage@gmail.com>
2018-10-14 11:50:58 -0700

    audio controls (#4)
    Audio Wadio Sadio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-
 import './App.css';
 import MusicWidget from "./components/MusicWidget";
+import arrow from "./audio/arrow_x.wav";
+import baseball from "./audio/baseball_hit.wav";
+import boing from "./audio/boing_x.wav";
 
 class App extends React.Component {
+
   render() {
+    const songs = new Map();
+    songs.set('arrow', arrow);
+    songs.set('baseball', baseball);
+    songs.set('boing', boing);
     return (
-      <div className="App" style={{marginTop: "250px"}}>
-        <MusicWidget />
+
+      <div className="App"  style={{marginTop: "250px"}}>
+        <MusicWidget sourceMap={songs} />
       </div>
     );
   }
--- src/components/MusicWidget.js
+++ src/components/MusicWidget.js
@@ -1,20 +1,88 @@
 import React from 'react';
-import {MdPlayArrow, MdFastForward} from 'react-icons/md'
-
+import {MdFastForward, MdPlayArrow} from 'react-icons/md'
 import "../styles/MusicWidget.css"
+import AudioTrack from "../model/AudioTrack";
+import TrackItem from "./TrackItem";
 
 class MusicWidget extends React.Component {
-
   constructor(props) {
     super(props);
+    this.tracks = [];
+    this.audioRef = React.createRef(); // need ref to change audio source
+    this.toggleAudio = this.toggleAudio.bind(this);
+    this.playNow = this.playNow.bind(this);
+    this.createPlayButton = this.createPlayButton.bind(this);
+    this.ended = this.ended.bind(this);
+    this.renderPlaylist = this.renderPlaylist.bind(this);
+    this.createTracks(props.sourceMap);
+    this.state = {
+      isPlaying: false
+    };
+  }
+
+  createTracks(sources = new Map()) {
+    sources.forEach((src, name) => {
+      this.tracks.push(new AudioTrack(name, src))
+    })
+  }
+
+  renderPlaylist() {
+   return  this.tracks.map((audioTrack, index) =>
+        <TrackItem key={index} onClick={this.playNow} src={audioTrack.src} name={audioTrack.name}/>
+    );
+  }
+
+  createPlayButton() {
+    return (
+      <button className="circular-button" onClick={this.toggleAudio}>
+        {this.state.isPlaying ? <span>&#9612;&#9612;</span> : <span>&#x25b6;</span>}
+      </button>
+    );
+  }
+
+  playNow(audioTrack) {
+    const audioPlayer = this.audioRef.current;
+    audioPlayer.src = audioTrack.src;
+    audioPlayer.play();
+    this.setState({isPlaying: true})
+  }
+
+  // TODO: play button should only toggle
+  // logic for toggling  audio should not exist here
+  // the func that determines if audio exist should also
+  // emit event when no audio is available
+  toggleAudio() {
+    const audioPlayer = this.audioRef.current;
+    if (!audioPlayer.paused) {
+      audioPlayer.pause();
+      this.setState({isPlaying: false});
+      return;
+    }
+    audioPlayer.play();
+    this.setState({isPlaying: true});
+  }
+
+
+  // TODO: implement seek? use HTMLMediaElement.currentTime + slider?
+  seeking(event) {
+
+  }
+
+  ended() {
+    this.setState({isPlaying: false})
   }
 
   render() {
     return (
-        <div className="music-widget-container">
+        <div>
+          <audio ref={this.audioRef} src={this.props.sourceMap.get('arrow')} onEnded={this.ended}/>
+          {this.createPlayButton()}
+          {this.renderPlaylist()}
+          <div className="music-widget-container">
 
             <div>
-              <button className="circular-button fast-forward-button mirror-content" onClick={()=>{}}>
+              <button className="circular-button fast-forward-button mirror-content" onClick={() => {
+              }}>
                 <MdFastForward className="inner-icon"/>
               </button>
             </div>
@@ -26,10 +94,12 @@ class MusicWidget extends React.Component {
             </div>
 
             <div>
+
               <button className="circular-button fast-forward-button" onClick={()=>{}}>
                 <MdFastForward className="inner-icon"/>
               </button>
             </div>
+          </div>
         </div>
     )
   }
